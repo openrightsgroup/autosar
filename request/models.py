f5b697b06ab0a08ed20b4a9b79bcaa1f469b4ac1
Jinn Koriech <jinn@ixydo.com>
2014-10-04 18:01:59 +0100

    Fix the details iterator in the template.

--- request/models.py
+++ request/models.py
@@ -11,10 +11,10 @@ class NameForm(forms.Form):
   address = forms.CharField(label='Address', max_length=100, initial="address")
   date = forms.CharField(label='date', max_length=100, initial=date.today() )
 
-  details = {
+  details = [
       forms.BooleanField( label="YOUR PERSONNEL FILE",),
-      forms.BooleanField( label="emails between ‘A’ and ‘B’ (between 1/6/11 and 1/9/11);",),
-      forms.BooleanField( label="your medical records (between 2006 & 2009) held by Dr ‘C’ at ‘D’ hospital;",),
-      forms.BooleanField( label="CCTV camera situated at (‘E’ location) on 23/5/12 between 11am and 5pm;",),
+      forms.BooleanField( label="emails between A and B (between 1/6/11 and 1/9/11);",),
+      forms.BooleanField( label="your medical records (between 2006 & 2009) held by Dr C at D hospital;",),
+      forms.BooleanField( label="CCTV camera situated at (E location) on 23/5/12 between 11am and 5pm;",),
       forms.BooleanField( label="copies of statements (between 2006 & 2009) held in account number xxxxx).",),
-      }
+      ]
